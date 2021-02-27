8ae0628660c655e03058bb404426a178db9780a8
Mina Tahaei <minatahaei@outlook.com>
2021-02-27 14:53:53 +0330

    add additional metrics

--- NewImplementation.py
+++ NewImplementation.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import math
 import operator
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 data = pd.read_csv('iris.csv')
 
@@ -47,19 +51,34 @@ def knn(trainingSet, testInstance, k):
 testSet = [[7.2, 3.6, 5.1, 2.5]]
 test = pd.DataFrame(testSet)
 
+
 print('\n\nWith 1 Nearest Neighbour \n\n')
 k = 1
 result, neigh = knn(data, test, k)
 print('\nPredicted Class of the datapoint = ', result)
 print('\nNearest Neighbour of the datapoints = ', neigh)
+
 print('\n\nWith 3 Nearest Neighbours\n\n')
-# Setting number of neighbors
 k = 3
 result, neigh = knn(data, test, k)
 print('\nPredicted class of the data = ', result)
 print('\nNearest Neighbours of the data = ', neigh)
+
 print('\n\nWith 5 Nearest Neighbours\n\n')
 k = 5
 result, neigh = knn(data, test, k)
 print('\nPredicted class of the data = ', result)
 print('\nNearest Neighbours of the data = ', neigh)
+
+# accuracy: (tp + tn) / (p + n)
+# accuracy = accuracy_score(test, data)
+# print('Accuracy: %f' % accuracy)
+# precision tp / (tp + fp)
+# precision = precision_score(test, data)
+# print('Precision: %f' % precision)
+# recall: tp / (tp + fn)
+# recall = recall_score(test, data)
+# print('Recall: %f' % recall)
+# f1: 2 tp / (2 tp + fp + fn)
+# f1 = f1_score(test, data)
+# print('F1 score: %f' % f1)
